c885c3f8d2aeaf819ab7a206c6adcd7154d765fa
vivekanandan127 <vvk90022@gmail.com>
2026-05-23 09:54:43 +0530

    updated files

--- upload.js
+++ upload.js
@@ -224,7 +224,6 @@ document.getElementById(
                     imageInput.files[0]
                 );
 
-                console.log(req.file);
 
                 // DEBUG
                 console.log(
